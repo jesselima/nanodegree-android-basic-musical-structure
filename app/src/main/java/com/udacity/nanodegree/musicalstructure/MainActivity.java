bd50125100a9a4e6c927063e25b9046046db45c8
jesselima <jesselima.tech@gmail.com>
2018-03-08 14:44:07 -0300

    Testing parsing json from api with volley. Tutorial from Code in Flow: https://www.youtube.com/watch?v=y2xtLqP8dSQ https://codinginflow.com/code-examples/android/volley-json-parsing

--- app/src/main/java/com/udacity/nanodegree/musicalstructure/MainActivity.java
+++ app/src/main/java/com/udacity/nanodegree/musicalstructure/MainActivity.java
@@ -2,60 +2,74 @@ package com.udacity.nanodegree.musicalstructure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
-    ArrayList<String> numberList = new ArrayList<>();
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadMusicData();
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
     }
 
-    public void loadMusicData(){
+    private void jsonParse() {
 
-        String json;
+        String url = "https://api.myjson.com/bins/kp9wz";
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("employees");
 
-        try {
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject employee = jsonArray.getJSONObject(i);
 
-            InputStream is = getAssets().open("xyz.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
+                                String firstName = employee.getString("firstname");
+                                int age = employee.getInt("age");
+                                String mail = employee.getString("mail");
 
-            json = new String(buffer, "UTF-8");
-            JSONArray jsonArray = new JSONArray(json);
-
-            for(int i = 0; i < jsonArray.length(); i++){
-
-                JSONObject obj = jsonArray.getJSONObject(i);
-
-                if (obj.getString("name").equals("Jesse")){
-
-                    numberList.add(obj.getString("number"));
-                    Toast.makeText(getApplicationContext(), numberList.toString(), Toast.LENGTH_SHORT).show();
-
-                }
+                                mTextViewResult.append(firstName + ", " + String.valueOf(age) + ", " + mail + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
             }
+        });
 
-        }catch (IOException e){
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        mQueue.add(request);
     }
 }
