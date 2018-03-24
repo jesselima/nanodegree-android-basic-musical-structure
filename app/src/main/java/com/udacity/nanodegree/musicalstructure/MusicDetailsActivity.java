37fe1366507d60c5996452a460df6aca94eef6e9
jesselima <jesselima.tech@gmail.com>
2018-03-23 22:46:41 -0300

    MusicDetails Load Artist Image from URL using glide.

--- app/src/main/java/com/udacity/nanodegree/musicalstructure/MusicDetailsActivity.java
+++ app/src/main/java/com/udacity/nanodegree/musicalstructure/MusicDetailsActivity.java
@@ -2,12 +2,17 @@ package com.udacity.nanodegree.musicalstructure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 
 public class MusicDetailsActivity extends AppCompatActivity {
 
-    static TextView textViewSong, textViewArtist, textViewYear, textViewGenres, textViewRhythm, textViewDuration, textViewAboutArtist;
+    TextView textViewSong, textViewArtist, textViewYear, textViewGenres, textViewRhythm, textViewDuration, textViewAboutArtist;
+    ImageView textViewArtistImage;
+    String urlArtistImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class MusicDetailsActivity extends AppCompatActivity {
         textViewRhythm = findViewById(R.id.text_view_song_rhythm);
         textViewDuration = findViewById(R.id.text_view_song_duration);
         textViewAboutArtist = findViewById(R.id.text_view_about_artist);
+        textViewArtistImage = findViewById(R.id.image_view_artist);
 
         Bundle musicData = getIntent().getExtras();
         textViewSong.setText(musicData.getString("song"));
@@ -30,6 +36,11 @@ public class MusicDetailsActivity extends AppCompatActivity {
         textViewRhythm.setText(musicData.getString("rhythm"));
         textViewDuration.setText(musicData.getString("duration"));
         textViewAboutArtist.setText(musicData.getString("aboutArtist"));
+
+        urlArtistImage = (musicData.getString("urlArtistImage"));
+        Glide.with(this)
+                .load(urlArtistImage)
+                .into(textViewArtistImage);
     }
 
 }
